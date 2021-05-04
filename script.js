2d3514a4b32b48c6d63ede64eb49ed95335104c4
jimbobding <jimmygrahamdavid@gmail.com>
2021-05-04 12:23:35 +0100

    added index, style and script -  basic

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("brush").innerHTML =
+    "Why is there a paint brush in the middle of whatever the fuck that is?!";
+}
+
+button.addEventListener("click", clicked);
