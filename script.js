178c514865713d7cba892c57e976d29c9dd25c1d
jimbobding <jimmygrahamdavid@gmail.com>
2021-05-07 12:08:57 +0100

    sorted padding on cards

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function myFunction() {
+function myFunction1() {
   document.getElementById("brush").innerHTML =
     "Why is there a paint brush in the middle of whatever the fuck that is?!";
 }
 
-button.addEventListener("click", clicked);
+function myFunction() {
+  document.getElementById("brush2").style.color = "red";
+}
+
+function show1() {
+  document.getElementById("show").style.display = "block";
+}
+
+function hide() {
+  document.getElementById("img").style.display = "none"; //hide
+}
