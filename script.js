143b48125265883fd6ebe966d51286c4449a6d04
jimbobding <jimmygrahamdavid@gmail.com>
2021-05-20 13:53:07 +0100

    fixed flip card

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ function myFunction1() {
     "Why is there a paint brush in the middle of whatever the fuck that is?!";
 }
 
+function myFunction2() {
+  document.getElementById("tired").innerHTML =
+    "I am far too tired to comment but looked like you got ripped off";
+}
+
+
+
+
+
+
+
 function myFunction() {
   document.getElementById("brush2").style.color = "red";
 }
@@ -13,4 +24,18 @@ function show1() {
 
 function hide() {
   document.getElementById("img").style.display = "none"; //hide
+}
+
+
+
+
+const card = document.getElementById("card");
+const card2 = document.getElementById("card2");
+
+card.addEventListener("click", cardFlip);
+card2.addEventListener("click", cardFlip);
+
+function cardFlip() {
+  card.classList.toggle("cardFlip");
+  card2.classList.toggle("cardFlip");
 }
